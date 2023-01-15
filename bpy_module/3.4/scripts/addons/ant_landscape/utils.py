4ffe7983acaf20182599eeaafb6dd84c3e3b68c5
Paul Artushkov <pavTiger@gmail.com>
2023-01-15 20:14:12 +0300

    Remove bpy build as pip's version is now fixed

--- bpy_module/3.4/scripts/addons/ant_landscape/utils.py
+++
@@ -1,8 +0,0 @@
-# SPDX-License-Identifier: GPL-2.0-or-later
-
-numexpr_available = False
-try:
-    import numexpr
-    numexpr_available = True
-except ImportError:
-    pass
--- bpy_module/3.4/scripts/addons/object_collection_manager/persistent_data.py
+++
@@ -1,7 +0,0 @@
-# SPDX-License-Identifier: GPL-2.0-or-later
-
-# Copyright 2011, Ryan Inch
-
-# QCD
-slots = {}
-overrides = set()
